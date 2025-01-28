5a9a2905896e148a3501886ec95e6ffe452568cf
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-28 16:22:04 +0200

    First commit

---
+++ touch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+
+	std::ofstream fileName(argv[1]);
+	if (argc == 1) {
+		std::cout << "Error: No arguments passed." << std::endl;
+		return 1;
+	} else if (argc == 2){
+		std::cout << "Wrote to a file: " << argv[1];
+		return 0;
+	} else {
+		std::cout << "touch.exe: Unexpected error.";
+		return 1;
+	}
+
+	return 0;
+}
